0cd236420c5932cfdad61c37c09ec15e1e31f93c
AlfonsoCorona1996 <alfonso.corona1996@gmail.com>
2022-11-07 22:27:30 -0600

    detalles de cotizacion empezada, header completo

--- src/app/inicio/cliente/cotizaciones/mis-cotizaciones/mis-cotizaciones.component.ts
+++ src/app/inicio/cliente/cotizaciones/mis-cotizaciones/mis-cotizaciones.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { cot_list} from 'src/app/interfaces/cotizaciones.interfaces';
+import { cot_list } from 'src/app/interfaces/cotizaciones.interfaces';
 import { CotizacionesClienteService } from 'src/app/services/cotizaciones-cliente.service';
 import { CotizacionesPipePipe } from '../../cotizaciones-pipe.pipe';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { cot_larga } from '../../../../interfaces/cotizaciones.interfaces';
 
 interface options {
   value: string;
@@ -21,9 +23,33 @@ export class MisCotizacionesComponent implements OnInit {
   public solicitada: number = 0;
   public proceso: number = 0;
   public entregada: number = 0;
-  public buscador:string = '';
+  public buscador: string = '';
   public filter_list: string = '';
   public filter_option: string = 'folio';
+  public empresa: string = '';
+  public status: string = '';
+  public today: Date = new Date();
+  public diferencia: string = ""
+  public cotizacion: cot_larga = {
+    _id: '',
+    folio: '',
+    des_corta: '',
+    des_larga: '',
+    empresa: '',
+    sitio: '',
+    equipos: [],
+    atencion: '',
+    remitente: '',
+    fecha_sol: new Date(),
+    fecha_pro: new Date(),
+    fecha_cot: new Date(),
+    fecha_cad: new Date(),
+    doc: '',
+    vigencia: '',
+    status: '',
+    __v: 0,
+
+  }
 
   constructor(
     private _cotizaciones_clienteService: CotizacionesClienteService,
@@ -31,31 +57,34 @@ export class MisCotizacionesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
     this._cotizaciones_clienteService.listar_cotizaciones_cliente().subscribe({
       next: (v) => {
 
         this.cotizaciones = v;
-        if(this.cotizaciones != undefined){
-        for (let stat of this.cotizaciones) {
+        if (this.cotizaciones != undefined) {
+          for (let stat of this.cotizaciones) {
 
-          if (stat.status === 'Entregada') {
+            if (stat.status === 'Entregada') {
               this.entregada += 1
-          } else if (stat.status === 'Solicitada') {
-            this.solicitada += 1
+            } else if (stat.status === 'Solicitada') {
+              this.solicitada += 1
 
-          } else if (stat.status === 'Proceso') {
-            this.proceso += 1
-          }else{
-            this.caducada += 1
+            } else if (stat.status === 'Proceso') {
+              this.proceso += 1
+            } else {
+              this.caducada += 1
+            }
           }
-        }
-        console.log( this.cotizaciones)
 
-      }
+        }
       },
       error: (e) => console.error(e)
     });
+
+    const token = localStorage.getItem('token') || undefined;
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(token);
+    this.empresa = decodedToken.empresa;
 
 
   }
@@ -64,18 +93,109 @@ export class MisCotizacionesComponent implements OnInit {
   Search(search: string) {
     // this.page = 0;
     this.filter_list = search;
-    this.solicitada = this.CotizacionesPipe.transform(this.cotizaciones,'Solicitada',this.filter_option,search ).length
-    this.entregada = this.CotizacionesPipe.transform(this.cotizaciones,'Entregada',this.filter_option,search ).length
-    this.proceso = this.CotizacionesPipe.transform(this.cotizaciones,'Proceso',this.filter_option,search ).length
-    this.caducada = this.CotizacionesPipe.transform(this.cotizaciones,'Caduco',this.filter_option,search ).length
+    this.solicitada = this.CotizacionesPipe.transform(this.cotizaciones, 'Solicitada', this.filter_option, search).length
+    this.entregada = this.CotizacionesPipe.transform(this.cotizaciones, 'Entregada', this.filter_option, search).length
+    this.proceso = this.CotizacionesPipe.transform(this.cotizaciones, 'Proceso', this.filter_option, search).length
+    this.caducada = this.CotizacionesPipe.transform(this.cotizaciones, 'Caduco', this.filter_option, search).length
   }
 
-  select(opcion:string){
+  select(opcion: string) {
     // if(opcion != 'Fecha' && this.filter_list != ''){
     //   this.filter_list = ''
     //   this.buscador = ''
     // }
     this.filter_option = opcion
+  }
+
+  Details(folio: string) {
+    this._cotizaciones_clienteService.listar_cotizacion_cliente_largas(folio).subscribe({
+      next: (v) => {
+        this.cotizacion = v[0];
+        console.log(this.cotizacion);
+        this.details_();
+        this.time_between();
+      },
+      error: (e) => console.error(e)
+    })
+  }
+
+  details_() {
+    const color = document.getElementById("miscotizaciones-status") as HTMLInputElement | null;
+    if (this.cotizacion.status == "Solicitada") {
+      this.status = "Solicitado"
+      color?.setAttribute("style", "  background-color: #0086d4;");
+      var fechaInicio = new Date(this.cotizacion.fecha_sol).getTime();
+      var fechaFin = new Date().getTime();
+      if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) < 2) {
+        if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) == 1) {
+          this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " día")
+        } else {
+          this.diferencia = (" - hoy")
+        }
+      } else {
+        this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " días")
+      }
+    }
+    else if (this.cotizacion.status == "Proceso") {
+      this.status = "En Proceso"
+      color?.setAttribute("style", "  background-color: #0a9ea1;");
+      var fechaInicio = new Date(this.cotizacion.fecha_pro).getTime();
+      var fechaFin = new Date().getTime();
+      if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) < 2) {
+        if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) == 1) {
+          this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " día")
+        } else {
+          this.diferencia = (" - hoy")
+        }
+      } else {
+        this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " días")
+      }
+    }
+    else if (this.cotizacion.status == "Entregada") {
+      this.status = "Entregado"
+      color?.setAttribute("style", "  background-color: #14b76e;");
+      var fechaInicio = new Date(this.cotizacion.fecha_cot).getTime();
+      var fechaFin = new Date().getTime();
+      if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) < 2) {
+        if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) == 1) {
+          this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " día")
+        } else {
+          this.diferencia = (" - hoy")
+        }
+      } else {
+        this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " días")
+      }
+    }
+    else {
+      this.status = "Caducado"
+      color?.setAttribute("style", "  background-color: #e03f32;");
+      var fechaInicio = new Date(this.cotizacion.fecha_cad).getTime();
+      var fechaFin = new Date().getTime();
+      if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) < 2) {
+        if (Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) == 1) {
+          this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " día")
+        } else {
+          this.diferencia = (" - hoy")
+        }
+      } else {
+        this.diferencia = (" - hace " + Math.floor(((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24))) + " días")
+      }
+    }
+  }
+
+  time_between() {
+
+  }
+
+  uncheck() {
+    if (this.cotizaciones) {
+      for (let check of this.cotizaciones) {
+        if (check.folio) {
+          const box = document.getElementById(check.folio) as HTMLInputElement | null;
+          if (box != null) { box.checked = false; }
+        }
+      }
+    }
   }
 
   Opciones: options[] = [
